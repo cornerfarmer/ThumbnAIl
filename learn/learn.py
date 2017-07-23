31558eb30fbf3b082f2345528fe144ce8168f8cc
domin1101 <dominik.winkelbauer@outlook.de>
2017-07-23 20:38:26 +0200

    Switches to softmax and classes

--- learn/learn.py
+++ learn/learn.py
@@ -4,14 +4,14 @@ import model.Config
 model.Config.db_is_local = True
 
 import tensorflow as tf
-from thumbnails import ThumbnailDataset
+from thumbnails import ThumbnailDataset, NormalizedThumbnailDataset
 
-dataset = ThumbnailDataset()
+dataset = NormalizedThumbnailDataset(split_in_classes=True)
 
 
 def image_reader(queue):
     image_reader = tf.WholeFileReader()
-    _, image_record = image_reader.read(image_queue)
+    _, image_record = image_reader.read(queue)
 
     image = tf.image.decode_image(image_record)
     image_crop = tf.reshape(tf.image.crop_to_bounding_box(image, 11, 0, 90 - 11 * 2, 120), (68, 120, 3))
@@ -25,10 +25,10 @@ with tf.variable_scope("input_pipeline"):
 
     label_queue = tf.train.input_producer(dataset.labels, shuffle=False)
 
-    batch_size = 32
+    batch_size = 64
     min_after_dequeue = 10000
     capacity = min_after_dequeue + 3 * batch_size
-    batch_image, batch_label = tf.train.shuffle_batch([image_crop, label_queue.dequeue()], batch_size, capacity, min_after_dequeue)
+    batch_image, batch_label = tf.train.shuffle_batch([image_crop, label_queue.dequeue()], batch_size, capacity=capacity, min_after_dequeue=min_after_dequeue)
 
 
 def network(input, reuse=False):
@@ -45,18 +45,26 @@ def network(input, reuse=False):
         pool2_flat = tf.reshape(pool2, [-1, 120 / 4 * 68 / 4 * 64])
 
         dense = tf.layers.dense(pool2_flat, 1024, tf.sigmoid)
-        y = tf.layers.dense(dense, 1)
-        y_sigm = tf.sigmoid(y)
+        y = tf.layers.dense(dense, len(dataset.labels[0]))
+        if dataset.split_in_classes:
+            y_sigm = tf.nn.softmax(y)
+        else:
+            y_sigm = tf.sigmoid(y)
 
         return y, y_sigm
 
 y, y_sigm = network(batch_image)
 
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch_label, logits=y))
+if dataset.split_in_classes:
+    loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(onehot_labels=batch_label, logits=y))
+else:
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch_label, logits=y))
 opt = tf.train.AdamOptimizer().minimize(loss)
 
 test_filename = tf.placeholder(tf.string)
-test_image = tf.expand_dims(image_reader(test_filename), 0)
+test_image_queue = tf.FIFOQueue(capacity=1, dtypes=[tf.string], shapes=[()])
+enqueue_op = test_image_queue.enqueue(test_filename)
+test_image = tf.expand_dims(image_reader(test_image_queue), 0)
 _, test_output = network(test_image, True)
 
 sess = tf.Session()
@@ -73,7 +81,7 @@ merge_op = tf.summary.merge_all()
 
 print("Max: " + str(dataset.max_views))
 
-for i in xrange(100):
+for i in xrange(500):
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata = tf.RunMetadata()
     _, summary = sess.run([opt, merge_op], run_metadata=run_metadata, options=run_options)
@@ -83,8 +91,10 @@ for i in xrange(100):
 
 
 def test_video(video_id):
-    estimate = sess.run(test_output, feed_dict={test_filename: "/home/domin/Dokumente/ThumbnAIl/thumbs/" + video_id + ".jpg"})[0][0]
-    estimate *= dataset.max_views
+    estimate, _ = sess.run([test_output, enqueue_op], feed_dict={test_filename: "/home/domin/Dokumente/ThumbnAIl/thumbs/" + video_id + ".jpg"})
+    estimate = estimate[0]
+    print(estimate)
+    estimate = dataset.calculate_views_from_label(estimate if dataset.split_in_classes else estimate[0])
     real = dataset.get_view_count_for_video(video_id)
     print("Video " + video_id + ":  est: " + str(estimate) + "  real: " + str(real) + "  diff: " + str(estimate - real))
 
@@ -92,6 +102,9 @@ def test_video(video_id):
 test_video("_1PMlT8vmiA")
 test_video("4gSOMba1UdM")
 test_video("EeZsEh1GB2Q")
+
+for video_set in dataset.videos:
+    test_video(video_set[0].identifier)
 
 exit()
 
--- learn/thumbnails.py
+++ learn/thumbnails.py
@@ -1,12 +1,18 @@
+import random
+
+import peewee
+
 from model.Video import Video
 import numpy as np
+from math import log10
 
 class ThumbnailDataset:
 
-    def __init__(self):
+    def __init__(self, split_in_classes=False):
+        self.split_in_classes = split_in_classes
         self.videos = Video.select()
         self.next_index = 0
-        self.filenames, self.labels, self.max_views = self._videos_to_training_data(self.videos)
+        self._videos_to_training_data(self.videos)
 
     def next_batch(self, batch_size):
         if self.next_index + batch_size < len(self.videos):
@@ -20,16 +26,62 @@ class ThumbnailDataset:
         return self._videos_to_training_data(batch)
 
     def _videos_to_training_data(self, videos):
-        labels = np.zeros((len(videos), 1), dtype=np.float32)
+        labels = np.zeros((len(videos) if not self.split_in_classes else sum(len(video_set) for video_set in videos), 1 if not self.split_in_classes else len(videos)), dtype=np.float32)
         filenames = []
-        for i, video in enumerate(videos):
-            labels[i][0] = video.viewCount
-            filenames.append('/home/domin/Dokumente/ThumbnAIl/thumbs/' + video.identifier + '.jpg')
+
+        video_index = 0
+        for j, video_set in enumerate(videos):
+            for video in video_set:
+                if self.split_in_classes:
+                    labels[video_index][j] = 1
+                else:
+                    labels[video_index][0] = self._get_label(video)
+                filenames.append('/home/domin/Dokumente/ThumbnAIl/thumbs/' + video.identifier + '.jpg')
+                video_index += 1
 
         max_views = labels.max()
         labels /= labels.max()
-        return [filenames, labels, max_views]
+        self.filenames, self.labels, self.max_views = filenames, labels, max_views
 
     def get_view_count_for_video(self, video_id):
         video = Video.select().where(Video.identifier == video_id).get()
         return video.viewCount
+
+    def _get_label(self, video):
+        return video.viewCount
+
+    def calculate_views_from_label(self, label):
+        return self.max_views * label
+
+class NormalizedThumbnailDataset(ThumbnailDataset):
+
+    def __init__(self, split_in_classes=False, min_per_set=100):
+        self.split_in_classes = split_in_classes
+        max_views = Video.select(peewee.fn.Max(Video.viewCount)).scalar()
+        self.videos = [[] for i in range(int(log10(max_views) + 1))]
+
+        for video in Video.select():
+            self.videos[int(log10(video.viewCount)) if video.viewCount > 0 else 0].append(video)
+
+        self.videos = [x for x in self.videos if not len(x) < min_per_set]
+        smallest_set = min([len(x) for x in self.videos])
+
+        self.videos = [random.sample(x, smallest_set) for x in self.videos]
+
+        self.next_index = 0
+        if self.split_in_classes:
+            self._videos_to_training_data(self.videos)
+        else:
+            self._videos_to_training_data([item for sublist in self.videos for item in sublist])
+
+    def _get_label(self, video):
+        return log10(video.viewCount)
+
+    def calculate_views_from_label(self, label):
+        if self.split_in_classes:
+            return pow(10, int(log10(self.videos[np.argmax(label)][0].viewCount)))
+        else:
+            return pow(10, self.max_views * label)
+
+
+
